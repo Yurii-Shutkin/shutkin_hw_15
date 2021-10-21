4113daea6bb4a0a34c2bd884abc008b04e842b51
Yurii-Shutkin <itorik@ukr.net>
2021-10-21 14:56:26 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,13 @@ const arr = [1, 2, 3, -1, -2, -3];
 
 function getPosNum (arr) {
     const posNumArr = [];
-    if (arr.length === 0) {
-        return 'Передан пустой массив';
-    }
+    if (arr.length === 0) return 'Передан пустой массив';
 
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > 0) {
-            posNumArr.push(arr[i]);
-        } else if (posNumArr.length === 0) {
-            return null; 
-        }
+        if (arr[i] > 0) posNumArr.push(arr[i]);
     }
-    return posNumArr;
+
+    return posNumArr.length === 0 ? null : posNumArr;
 }
 
 console.log (getPosNum(arr));
